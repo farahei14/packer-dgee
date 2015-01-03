dcd03684015d4baef428a494a6c1c8208ba7bef4
James HORLEY <farahei14@mail.pf>
2015-01-02 18:45:07 -1000

    ajout de profil

--- scripts/centos/vmware-tools.sh
+++ scripts/centos/vmware-tools.sh
@@ -1,3 +1,40 @@
-rpm --import http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-DSA-KEY.pub
-rpm --import http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub
-yum -y install vmware-tools-esx-nox
+ISO_MOUNT="/media"
+VMWARE_INSTALL_PL="/tmp/vmware/vmware-tools-distrib/vmware-install.pl"
+#VMWARE_REQUIRED_PACKAGES="build-essential"
+VMWARE_TOOLS_TMP="/tmp/vmware"
+VMWARE_TOOLS_ISO="/root/linux.iso"
+
+echo "Installing virtual machine tools for ${PACKER_BUILDER_TYPE}"
+echo "Mounting ${VMWARE_TOOLS_ISO} onto ${ISO_MOUNT}"
+mount -o loop ${VMWARE_TOOLS_ISO} ${ISO_MOUNT}
+
+echo "Extracting VMWare Tools to /tmp/vmware"
+mkdir -p ${VMWARE_TOOLS_TMP}
+
+echo "Extracting ${ISO_MOUNT}/VMwareTools-*.tar.gz to ${VMWARE_TOOLS_TMP}"
+tar xzf ${ISO_MOUNT}/VMwareTools-*.tar.gz -C ${VMWARE_TOOLS_TMP}
+
+echo "Unmounting ${VMWARE_TOOLS_ISO} from ${ISO_MOUNT}"
+umount ${ISO_MOUNT}
+
+echo "Running ${VMWARE_INSTALL_PL}"
+${VMWARE_INSTALL_PL} << EOF
+/usr/bin
+/etc/rc.d
+/etc/rc.d/init.d
+/usr/sbin
+/usr/lib/vmware-tools
+yes
+/usr/share/doc/vmware-tools
+yes
+yes
+no
+no
+no
+no
+EOF
+
+echo "Cleaning up ${VMWARE_TOOLS_ISO} and ${VMWARE_TOOLS_TMP}"
+rm ${VMWARE_TOOLS_ISO}
+rm -rf ${VMWARE_TOOLS_TMP}
+
