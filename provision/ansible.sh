350a903bbd7bec874c1972045568fa12dec02760
farahei14 <farahei14@mail.pf>
2015-02-14 14:28:55 -1000

    - modification de la partie provisionning pour inclure l'installation de   la clé privé vagrant (cela évite de s'authentifier à chaque fois pendant   les tests.

--- provision/ansible.sh
+++ provision/ansible.sh
@@ -9,6 +9,10 @@ yum -y install ansible sshpass
 # desactive verification de la verification des id d'hote pour le developpement
 sed -i -e 's/^#host_key_checking.*$/host_key_checking = False/' /etc/ansible/ansible.cfg
 
+# install vagrant public private_key
+wget https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant -O /home/vagrant/.ssh/id_rsa
+chmod 600 /home/vagrant/.ssh/id_rsa
+
 # install git
 yum -y install git
 
